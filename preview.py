1d03594b654cc67bfb521b9f24429fba4eeaf92d
HansMPrieto <hansmiguelpprieto@gmail.com>
2021-05-16 17:55:33 -0700

    Added export button

--- preview.py
+++ preview.py
@@ -6,7 +6,14 @@ def previewPress():
     label = tk.Label(root, text = "Preview button pressed")
     label.pack()
 
-previewButton = tk.Button(root, text = "Preview", command = previewPress, height = 2, width = 15)
+def exportPress():
+    label = tk.Label(root, text = "Export button pressed")
+    label.pack()
+
+previewButton = tk.Button(root, text = "Preview", command = previewPress, height = 3, width = 15)
 previewButton.pack()
+
+exportButton = tk.Button(root, text = "Export", command = exportPress, height = 3, width = 15)
+exportButton.pack()
 
 root.mainloop()
