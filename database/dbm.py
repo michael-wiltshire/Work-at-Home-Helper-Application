e0e8b0c874a9ead4acb1dd13b8131a5d10b3e6c7
Alex Anderson <alexandersonone@gmail.com>
2021-05-18 14:56:42 -0700

    Update the database to use a oo approach

--- database/dbm.py
+++ database/dbm.py
@@ -5,25 +5,51 @@ CIS 422 Group 1
 '''
 
 import sqlite3
+import datetime
 
-def connect_and_create(filename='timeworked.db': str) -> sqlite3.Cursor:
-    '''
-    Connects to a database file and returns a 'cursor' to that database.
-    This object is then thrown into other functions to do the various needed
-    operations
+class DatabaseManager:
 
-    '''
-    # Connect to the timeworked database file and get it's cursor
-    # which is required to operate on the file
-    con = sqlite3.connect('timeworked.db')
-    cur = con.cursor()
+    def __init__(self, job: str, filename: str='timeworked.db') -> sqlite3.Cursor:
+        '''Creates a connection to a database that will be interacted with for the rest
+        of the lifespan of the class'''
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS activities
-        (
+        # Jobs should be managed one at a time
+        self.job = job
 
-    ''')
-    return
+        # Connect to the timeworked database file and get it's cursor
+        # which is required to operate on the file
+        self.con = sqlite3.connect('timeworked.db')
+        self.cur = self.con.cursor()
 
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS activities
+            (
+             `job` VARCHAR(100) DEFAULT '',  
+             `desc` VARCHAR(100) DEFAULT '',  
+             `start_time` DATETIME NOT NULL,  
+             `durration` INT NOT NULL
+             )
+        ''')
+
+    def __del__(self):
+        '''Close the connection to the database on deletion'''
+        self.con.close()
+
+    def add_activity(self, desc: str, start: datetime.datetime, durration: datetime.timedelta):
+        self.cur.execute('''INSERT INTO activities
+            (job, desc, start_time, durration)
+            values
+            (?, ?, ?, ?)
+        ''', (self.job, desc, start, durration.total_seconds()))
+
+        self.con.commit()
+
+    def debug_get_all(self):
+        self.cur.execute('SELECT * FROM activities')
+        print(self.cur.fetchall())
+        
+
+        
+        
 
 
     
