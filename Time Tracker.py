63066256d0973ee8fe207d20529adb53d9abafbd
Michael Wiltshire <mikulwinter@gmail.com>
2021-05-26 16:56:13 -0700

    add main func and color

--- Time Tracker.py
+++ Time Tracker.py
@@ -11,53 +11,51 @@ import time
 
 #def create_window(string job):
 
-
-root = Tk()
-root.title('how to get text from textbox')
-
-
-
-class time():
-    def __init__(self):
-        self.start_time = datetime.now()
-        self.stop_time = datetime.now()
-        self.description = ""
-        self.elapsed_time = 0
-
-# global start_time
-# global stop_time
-#date = datetime.datetime.now()
-#print(date)
-
-my_time = time()
-my_time.description = StringVar()
+def main():
+    root = Tk()
+    root.geometry("500x200")
+    root.title('Time Tracker')
 
 
 
-def clock_in():
-    my_time.start_time = datetime.now()
-    #print(description.get(), start_time)
-    #print(my_time.start_time)
-    # print(date)
-    # date = datetime.datetime.now()
-    # print(date)
-
-def clock_out():
-    my_time.stop_time = datetime.now()
-    #print(my_time.stop_time)
-
-def calculate_time():
-    print(my_time.description.get(),"start time:", my_time.start_time,"\n","stop time:", my_time.stop_time, "\n")
-    #calculates elapsed time
-    my_time.elapsed_time = (my_time.stop_time - my_time.start_time)
-    print("total time:",my_time.elapsed_time, "\n")
-    #db = dbm.DatabaseManager(my_time.description)
+    class time():
+        def __init__(self):
+            self.job = "job"
+            self.start_time = datetime.now()
+            self.stop_time = datetime.now()
+            self.description = ""
+            self.elapsed_time = 0
 
 
-Label(root, text="Job Description").grid(row=0, sticky=W)  #label
-Entry(root, textvariable = my_time.description).grid(row=0, column=1, sticky=E) #entry textbox
+    my_time = time()
+    my_time.description = StringVar()
 
-startbutton = Button(root, text="Start Time", command=clock_in).grid(row=3, column=0, sticky=W) #button
-stopbutton = Button(root, text="Stop Time", command=clock_out).grid(row=3, column=1, sticky=W) #button
-calculate_time = Button(root, text="Save", command=calculate_time).grid(row=3, column=2, sticky=W)
-mainloop()
+
+
+    def clock_in():
+        my_time.start_time = datetime.now()
+        Button(root, text="Stop Time", command=clock_out,height=5,width=20,bg="red").place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    def clock_out():
+        my_time.stop_time = datetime.now()
+        #print(my_time.stop_time)
+        calculate_time()
+
+    def calculate_time():
+        print(my_time.description.get(),"start time:", my_time.start_time,"\n","stop time:", my_time.stop_time, "\n")
+        #calculates elapsed time
+        my_time.elapsed_time = (my_time.stop_time - my_time.start_time)
+        print("total time:",my_time.elapsed_time, "\n")
+        db = dbm.DatabaseManager(my_time.job)
+        id = db.add_activity(my_time.description, my_time.start_time, my_time.elapsed_time)
+        thisJob = db.get_activity(id)
+        print(thisJob)
+
+    Label(root, text="Job Description").place(anchor=NW)  #label
+    Entry(root, textvariable = my_time.description, width=50).place(relx=0.5, anchor=N) #entry textbox
+
+    Button(root, text="Start Time", command=clock_in,height=5,width=20,bg="green").place(relx=0.5, rely=0.5, anchor=CENTER)
+
+
+    mainloop()
+main()
