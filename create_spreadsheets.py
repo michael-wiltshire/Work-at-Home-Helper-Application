27c8035cf6dd559660e865162eb1a771259a487e
Vince Qiu <rexqiu5605@gmail.com>
2021-05-26 16:17:01 -0700

    update

--- create_spreadsheets.py
+++ create_spreadsheets.py
@@ -7,24 +7,18 @@ This file pulls information from the database and creates spreadsheet files.
 
 from database import dbm
 import datetime
-import sys
-import os
 import csv
-from io import StringIO
 
 
 def write_work_list(filename, db, start, end):
     start_datetime = datetime.datetime.combine(start, datetime.datetime.min.time())
     end_datetime = datetime.datetime.combine(end, datetime.datetime.min.time())
-    # db = dbm.DatabaseManager('CIS 422 TA')
     work = db.get_activities_within_range(start_datetime, end_datetime)
     print(work)
 
-    with open(filename, 'w', newline='') as csvfile:
+    with open(filename, 'w', newline='') as csv_file:
         fieldnames = ['Job_ID', 'Job', 'Description', 'Date', 'Start', 'End', 'Duration']
-        writer = csv.writer(csvfile)
-
-        # writer.writeheader()
+        writer = csv.writer(csv_file)
         writer.writerow(fieldnames)
 
         total = 0
@@ -32,21 +26,16 @@ def write_work_list(filename, db, start, end):
             print(activity)
             job_id = activity[0]
             print("job id:", job_id)
-            # job is [1]
             job = activity[1]
             print("job:", job)
             des = activity[2]
             print("description", des)
-            #print(len(activity[3]))
             date_t = activity[3]
             date = date_t.strftime("%m/%d/%Y")
             start = date_t.strftime("%I:%M %p")
             print("date", date)
             print("start:", start)
 
-            # duration = str(datetime.timedelta(seconds=activity[4]))
-            # duration_format = duration[:-3]
-            # print("duration:", duration_format)
             duration = str(round(activity[4] / 3600, 2))
 
             end = (date_t + datetime.timedelta(seconds=activity[4]))
@@ -57,12 +46,10 @@ def write_work_list(filename, db, start, end):
             tmp = [job_id, job, des, date, start, end_time, duration]
             writer.writerow(tmp)
 
-            # writer.writerow({'Job_ID': job_id, "Job": job, "Description": des,
-            #                  "Date": date, "Start": start, "End": end_time, "Duration": duration_format})
-
         total_time_format = str(round(total / 3600, 2))
         print("total time", total_time_format)
         writer.writerow([" ", " ", "total time", " ", " ", " ", total_time_format])
+
 
 def main():
     jobname = "sample job"
@@ -70,6 +57,7 @@ def main():
     start = datetime.date(2021, 5, 18)
     end = datetime.date(2021, 5, 31)
     write_work_list('timesheet.csv', db, start, end)
+
 
 if __name__=='__main__':
     main()
