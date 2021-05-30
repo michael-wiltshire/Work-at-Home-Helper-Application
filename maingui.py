74da5679f4a4f5fae94b903f58447ccefa1545b4
CSTrebbien <trebbiencolton@gmail.com>
2021-05-30 16:55:06 -0700

    error handler created

--- maingui.py
+++ maingui.py
@@ -14,6 +14,7 @@ import timetracker
 """
 Tkninter CONFIGURATION
 """
+
 root = Tk()
 root.title("WorkAtHomeHelper")
 root.geometry("510x600")
@@ -23,7 +24,8 @@ f=("Times", 20)
 """
 DATABASE CONFIGURATION
 """
-Jobname = "Learning Assistant"
+
+Jobname = "Learning Assistant" 					
 db = dbm.DatabaseManager(Jobname, 'JobDatabase.db')
 
 #Date Options
@@ -33,7 +35,7 @@ Frame1 = Frame(root, width= 800)
 Frame1.place(x=0, y=0, height=40, width = 800)
 
 TitleFont =("Times", 30) 
-Title = Label(Frame1, text= "WorkAtHomeHelper-V1", font=TitleFont)
+Title = Label(Frame1, text= "WorkAtHomeHelper", font=TitleFont)
 Title.place(x=0,y=0)
 #----------------------FRAMES TWO OBJECTS--------------------------
 # Freame Two is the Time module, and Preview.
@@ -83,7 +85,7 @@ PS_Y_ENTRY.place(x=50,y=60)
 PEFRAME = Frame(Frame2, width= 140)
 PEFRAME.place(x=350, y=45, height=95, width = 140)
 
-P_END_LABEL = Label(PEFRAME, text="END DATE (M/D/Y)")
+P_END_LABEL = Label(PEFRAME, text="End Date (M/D/Y)")
 P_END_LABEL.place(x=0,y=0)
 
 E_MONTH = Label(PEFRAME, text="Month:")
@@ -101,6 +103,9 @@ PE_M_ENTRY.place(x=50,y=20)
 PE_D_ENTRY.place(x=50,y=40)
 PE_Y_ENTRY.place(x=50,y=60)
 
+
+
+
 #==============================================
 
 def PreviewWindowOpener():
@@ -110,9 +115,9 @@ def PreviewWindowOpener():
 	s_day = (PS_D_ENTRY.get())	#START DAY
 	s_year = (PS_Y_ENTRY.get())	#START YEAR
 	#GET END DATE
-	e_month = (PS_M_ENTRY.get()) #END MONTH
-	e_day = (PS_D_ENTRY.get())	#END DAY
-	e_year = (PS_Y_ENTRY.get())	#END YEAR
+	e_month = (PE_M_ENTRY.get()) #END MONTH
+	e_day = (PE_D_ENTRY.get())	#END DAY
+	e_year = (PE_Y_ENTRY.get())	#END YEAR
 	if ((s_month or s_day or s_year or e_month or e_day or e_year) == ''):
 		ps_default = datetime.date(1,1,1)
 		pe_default = datetime.date(3000,1,1)
@@ -153,20 +158,20 @@ F3_MANENTRY_LABEL.place(x= 0,y=0)
 
 #Frame 3 Labels
 
-F3_JOBNAME_LABEL=Label(Frame3, text="Job Name:")
-F3_JOBNAME_LABEL.place(x=0,y=20)
+#F3_JOBNAME_LABEL=Label(Frame3, text="Job Name:")
+#F3_JOBNAME_LABEL.place(x=0,y=20)
 F3_JOBDESC_LABEL=Label(Frame3, text="Description:")
-F3_JOBDESC_LABEL.place(x=0,y=45)
+F3_JOBDESC_LABEL.place(x=0,y=20)
 
 #ENTRYS
-F3_JOBENTRY = Entry(Frame3,width = 60)
-F3_JOBENTRY.place(x=80,y=20)
+#F3_JOBENTRY = Entry(Frame3,width = 60)
+#F3_JOBENTRY.place(x=80,y=20)
 F3_DESCENTRY = Entry(Frame3,width = 60)
-F3_DESCENTRY.place(x=80,y=45)
+F3_DESCENTRY.place(x=80,y=20)
 
 #START DATE FRAME================================================
 SDFrame = Frame(Frame3, width= 140)
-SDFrame.place(x=0, y=90, height=80, width = 140)
+SDFrame.place(x=0, y=60, height=80, width = 140)
 
 F3_STARTDATE_LABEL = Label(SDFrame, text="Start Date (M/D/Y)")
 F3_STARTDATE_LABEL.place(x=0,y=0)
@@ -188,7 +193,7 @@ SD_Y_ENTRY.place(x=50,y=60)
 
 #START TIME FRAME===============================================
 STARTFrame = Frame(Frame3, width= 200)
-STARTFrame.place(x=170, y=90, height=70, width = 200)
+STARTFrame.place(x=170, y=60, height=70, width = 200)
 
 IST =Label(STARTFrame, text="Intial Start Time (H:M AM/PM)")
 IST.place(x=0,y=0)
@@ -234,7 +239,7 @@ SF_sec_hour.place(x=0,y=20)
 
 #Time FRAME--------------------------
 TFrame = Frame(Frame3, width= 120)
-TFrame.place(x=400, y=90, height=100, width = 120)
+TFrame.place(x=400, y=60, height=100, width = 120)
 
 F3_DURATION_LABEL=Label(TFrame, text="Time Worked")
 F3_DURATION_LABEL.place(x=0,y=0)
@@ -264,8 +269,8 @@ T_MINS.place(x=60,y=55)
 IST =Label(STARTFrame, text="Intial Start Time")
 IST.place(x=0,y=0)
 
-min_sb.place(x=0,y=20)
-sec_hour.place(x=0,y=50)
+min_sb.place(x=0,y=50)
+sec_hour.place(x=0,y=20)
 
 def Submitbutton():
 	print("SUBMIT BUTTON PRESSED")
@@ -275,8 +280,8 @@ def Submitbutton():
 	sub_sded = SD_D_ENTRY.get()
 	sub_sdey = SD_Y_ENTRY.get()
 
-	in_h = SF_min_sb.get()
-	in_m = SF_sec_hour.get()
+	in_m = SF_min_sb.get()
+	in_h = SF_sec_hour.get()
 
 	t_m = min_sb.get()
 	t_h = sec_hour.get()
@@ -285,15 +290,83 @@ def Submitbutton():
 
 	if(M_PM == "PM"):
 		in_h = int(in_h)+12
-	
 
-	print(f"{sub_jobdesc} {sub_sdey}/{sub_sdem}/{sub_sded} IT: {in_h}:{in_m} {M_PM} TW: {t_h}:{t_m} ")
+	#print(f"{sub_jobdesc} {sub_sdey}/{sub_sdem}/{sub_sded} IT: {in_h}:{in_m} {M_PM} TW: {t_h}:{t_m} ")
+	#UPDATE THE STUFF
+	NoteLabel.config(text="Manual Date Entered")
+	try:
+		
+	
+		U_desc = "Desc: " + str(sub_jobdesc)
+		U_date = "Date: " + str(datetime.datetime(int(sub_sdey), int(sub_sdem), int(sub_sded),int(in_h), int(in_m)))
+		U_tw = "Timeworked: " + str(datetime.timedelta(hours=int(t_h) ,minutes=int(t_m)))
+
+		Update_label_desc.config(text= U_desc)
+		Update_label_date.config(text=U_date)
+		Update_label_timework.config(text=U_tw)
+
+
+		db.add_activity(sub_jobdesc , datetime.datetime(int(sub_sdey), int(sub_sdem), int(sub_sded),int(in_h), int(in_m)),
+	 	 datetime.timedelta(hours=int(t_h) ,minutes=int(t_m)))
+
+	except:
+		U_desc = "ERROR: Couldnt Process Input"
+		U_date = ""
+		U_tw = ""
+
+		Update_label_desc.config(text= U_desc)
+		Update_label_date.config(text=U_date)
+		Update_label_timework.config(text=U_tw)
+
+
+
+
+	"""
+	try:
+		NoteLabel.config(text="Manual Date Entered")
+		if((sub_jobdesc or sub_sdey or sub_sdem or sub_sded) == ''):
+			U_desc = "ERROR: Couldnt Process Input"
+			U_date = ""
+			U_tw = ""
+			print("no feild entered")
+
+		else:
+			U_desc = "Desc: " + str(sub_jobdesc)
+			U_date = "Date: " + str(datetime.datetime(int(sub_sdey), int(sub_sdem), int(sub_sded),int(in_h), int(in_m)))
+			U_tw = "Timeworked: " + str(datetime.timedelta(hours=int(t_h) ,minutes=int(t_m)))
+	except:
+		U_desc = "ERROR: Couldnt Process Input"
+		U_date = ""
+		U_tw = ""
+	"""
+
+	"""
+	Update_label_desc.config(text= U_desc)
+	Update_label_date.config(text=U_date)
+	Update_label_timework.config(text=U_tw)
 
 
 	db.add_activity(sub_jobdesc , datetime.datetime(int(sub_sdey), int(sub_sdem), int(sub_sded),int(in_h), int(in_m)),
 	 datetime.timedelta(hours=int(t_h) ,minutes=int(t_m)))
+	 """
 
 F3_submit = Button(Frame3, text="Submit Time", command = Submitbutton,height=3, width=10)
-F3_submit.place(x=0, y=180)
+F3_submit.place(x=200, y=150)
+
+
+NoteFrame = Frame(Frame2, width= 290, borderwidth = 2, relief = "ridge")
+NoteFrame.place(x=200, y= 143, height=97, width = 290)
+
+NoteLabel = Label(NoteFrame, text="Input Display")
+NoteLabel.place(x=0,y=0)
+
+Update_label_desc = Label(NoteFrame, text="")
+Update_label_desc.place(x=0,y =20)
+Update_label_date = Label(NoteFrame, text="")
+Update_label_date.place(x=0,y =40)
+
+Update_label_timework = Label(NoteFrame, text="")
+Update_label_timework.place(x=0,y=60)
+
 
 root.mainloop()
